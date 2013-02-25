6a3798b9c4e5133d8c159bcafb4718e9535ff260
patrick aubin <paubin12@gmail.com>
2013-02-25 12:41:36 -0600

    updated events

--- cap_events/templatetags/get_events.py
+++ cap_events/templatetags/get_events.py
@@ -4,9 +4,17 @@ register = template.Library()
 
 from cap_events.models import Event
 
+import datetime
+
 @register.assignment_tag
-def get_events(tag_name=None):
+def get_all_events():
     events = Event.objects.all()
+    return events
+
+
+@register.assignment_tag
+def get_events():
+    events = Event.objects.filter(date__gte=datetime.datetime.now())
     return events
 
 
